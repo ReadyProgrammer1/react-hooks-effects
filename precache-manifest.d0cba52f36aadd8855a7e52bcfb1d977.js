2014e2f7b6eb187bb31fa85d10855435fba12bb4
Daniel Payne <42071158+ReadyProgrammer1@users.noreply.github.com>
2018-11-02 11:07:17 -0400

    Add files via upload

---
+++ precache-manifest.d0cba52f36aadd8855a7e52bcfb1d977.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f59de14e6f4c8d2de28dc68d4b9bdde5",
+    "url": "/react-hooks-effects/static/media/recipe.f59de14e.png"
+  },
+  {
+    "revision": "749e4548e3f4900d7152",
+    "url": "/react-hooks-effects/static/js/runtime~main.749e4548.js"
+  },
+  {
+    "revision": "8354c9a3350b14dd5bca",
+    "url": "/react-hooks-effects/static/js/main.8354c9a3.chunk.js"
+  },
+  {
+    "revision": "449f3182d0edcd2d0f38",
+    "url": "/react-hooks-effects/static/js/1.449f3182.chunk.js"
+  },
+  {
+    "revision": "8354c9a3350b14dd5bca",
+    "url": "/react-hooks-effects/static/css/main.888f31b3.chunk.css"
+  },
+  {
+    "revision": "18e56a112e219994fe1913a0a81b61d2",
+    "url": "/react-hooks-effects/index.html"
+  }
+];
